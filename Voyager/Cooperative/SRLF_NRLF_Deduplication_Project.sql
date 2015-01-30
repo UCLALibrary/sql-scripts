ffc7c2baeb414132ae1959ba434c4279e96c59bb
Andy Kohler <akohler@library.ucla.edu>
2015-01-30 11:36:08 -0900

    RR-44: Added query to extract bib 008 field

--- Voyager/Cooperative/SRLF_NRLF_Deduplication_Project.sql
+++ Voyager/Cooperative/SRLF_NRLF_Deduplication_Project.sql
@@ -48,3 +48,17 @@ inner join item_barcode ib on i.item_id = ib.item_id and ib.barcode_status = 1 -
 where (l.location_code in ('sr', 'srbuo') or l.location_code like 'srucl%')
 order by bib_format, lpad(oclc, 9, '0'), bib_id, mfhd_id, item_enum
 ;
+
+-- 2014-12-19: UCB realised they also need to konw if gov document, so requested
+-- bib id and bib 008 as separate file.
+-- Only need bib data, but must use loc and existence of items.
+select distinct
+  bt.bib_id
+, bt.field_008
+from bib_text bt
+inner join bib_mfhd bm on bt.bib_id = bm.bib_id
+inner join mfhd_master mm on bm.mfhd_id = mm.mfhd_id
+inner join location l on mm.location_id = l.location_id
+inner join mfhd_item mi on mm.mfhd_id = mi.mfhd_id
+where (l.location_code in ('sr', 'srbuo') or l.location_code like 'srucl%')
+;

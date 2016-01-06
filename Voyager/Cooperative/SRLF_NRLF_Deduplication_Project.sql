231e0f274c0bb88dc912e750c8bc6310dca0f213
Andy Kohler <akohler@library.ucla.edu>
2016-01-05 19:20:56 -0900

    RR-136: Updated query to add 008 field, included tsv export query

--- Voyager/Cooperative/SRLF_NRLF_Deduplication_Project.sql
+++ Voyager/Cooperative/SRLF_NRLF_Deduplication_Project.sql
@@ -1,14 +1,16 @@
 /*  Extract Voyager data for SRLF/NRLF de-duplication project
-    Specs: https://jira.library.ucla.edu/browse/RR-44
-    2014-11-26 akohler
+    Specs: https://jira.library.ucla.edu/browse/RR-136
+    2016-01-04 akohler
 */
 
+create table vger_report.tmp_srlf_rr136 as
 select
   bt.bib_id
 , bt.bib_format
+, bt.field_008
 , coalesce(bt.isbn, bt.issn) as isbn_or_issn
-, ( select replace(normal_heading, 'UCOCLC', '') 
-    from bib_index
+, ( select replace(normal_heading, 'UCOCLC', '')
+    from ucladb.bib_index
     where bib_id = bt.bib_id
     and index_code = '0350'
     and normal_heading like 'UCOCLC%'
@@ -18,7 +20,7 @@ select
 , vger_support.unifix(bt.title) as title
 , vger_support.unifix(bt.edition) as edition
 , vger_support.unifix(bt.imprint) as imprint
-, ( select subfield 
+, ( select subfield
     from vger_subfields.ucladb_bib_subfield
     where record_id = bt.bib_id
     and tag = '300a'
@@ -35,30 +37,49 @@ select
 , mi.freetext
 , istp.item_status_desc
 , ist.item_status_date
-from bib_text bt
-inner join bib_mfhd bm on bt.bib_id = bm.bib_id
-inner join mfhd_master mm on bm.mfhd_id = mm.mfhd_id
-inner join location l on mm.location_id = l.location_id
-inner join mfhd_item mi on mm.mfhd_id = mi.mfhd_id
-inner join item i on mi.item_id = i.item_id
-inner join item_type it on i.item_type_id = it.item_type_id
-inner join item_status ist on i.item_id = ist.item_id
-inner join item_status_type istp on ist.item_status = istp.item_status_type
-inner join item_barcode ib on i.item_id = ib.item_id and ib.barcode_status = 1 --Active
+from ucladb.bib_text bt
+inner join ucladb.bib_mfhd bm on bt.bib_id = bm.bib_id
+inner join ucladb.mfhd_master mm on bm.mfhd_id = mm.mfhd_id
+inner join ucladb.location l on mm.location_id = l.location_id
+inner join ucladb.mfhd_item mi on mm.mfhd_id = mi.mfhd_id
+inner join ucladb.item i on mi.item_id = i.item_id
+inner join ucladb.item_type it on i.item_type_id = it.item_type_id
+inner join ucladb.item_status ist on i.item_id = ist.item_id
+inner join ucladb.item_status_type istp on ist.item_status = istp.item_status_type
+inner join ucladb.item_barcode ib on i.item_id = ib.item_id and ib.barcode_status = 1 --Active
 where (l.location_code in ('sr', 'srbuo') or l.location_code like 'srucl%')
+;
+
+create index vger_report.tmp_srlf_rr136_ix on vger_report.tmp_srlf_rr136 (bib_format, lpad(oclc, 9, '0'), bib_id, mfhd_id, item_enum);
+
+-- Separate query (on server as export_srlf.sql) to export data as tab-delimited
+set linesize 32767
+set trimspool on
+set trimout on
+
+select
+    bib_id || chr(9)
+||  bib_format || chr(9)
+||  field_008 || chr(9)
+||  isbn_or_issn || chr(9)
+||  oclc || chr(9)
+||  author || chr(9)
+||  title || chr(9)
+||  edition || chr(9)
+||  imprint || chr(9)
+||  f300a || chr(9)
+||  language || chr(9)
+||  location_code || chr(9)
+||  location_name || chr(9)
+||  mfhd_id || chr(9)
+||  item_id || chr(9)
+||  item_barcode || chr(9)
+||  item_enum || chr(9)
+||  item_type_name || chr(9)
+||  freetext || chr(9)
+||  item_status_desc || chr(9)
+||  item_status_date
+from vger_report.tmp_srlf_rr136
 order by bib_format, lpad(oclc, 9, '0'), bib_id, mfhd_id, item_enum
 ;
 
--- 2014-12-19: UCB realised they also need to konw if gov document, so requested
--- bib id and bib 008 as separate file.
--- Only need bib data, but must use loc and existence of items.
-select distinct
-  bt.bib_id
-, bt.field_008
-from bib_text bt
-inner join bib_mfhd bm on bt.bib_id = bm.bib_id
-inner join mfhd_master mm on bm.mfhd_id = mm.mfhd_id
-inner join location l on mm.location_id = l.location_id
-inner join mfhd_item mi on mm.mfhd_id = mi.mfhd_id
-where (l.location_code in ('sr', 'srbuo') or l.location_code like 'srucl%')
-;

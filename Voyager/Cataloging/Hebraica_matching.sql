8d8f16af3eed73b792220ccaf53f03999ba35dcf
Andy Kohler <akohler@library.ucla.edu>
2016-09-16 11:47:18 -0700

    VBT-676: Extract item data for Hebraica scans

--- Voyager/Cataloging/Hebraica_matching.sql
+++ Voyager/Cataloging/Hebraica_matching.sql
@@ -53,4 +53,41 @@ order by oclc_number, bm.bib_id, bm.mfhd_id, mi.item_enum
 -- Clean up
 drop table vger_report.tmp_oclc_import purge;
 
-select count(distinct oclc_number) from vger_report.tmp_oclc_import;
+/*	Another version, done differently to get just item barcode and enum.
+	JIRA VBT-676
+	2016-09-16 akohler
+*/
+create table vger_report.tmp_hebraica_matches (
+  oclc_number varchar2(10) not null primary key
+);
+-- import 596 rows via sqlldr
+
+select
+  h.oclc_number
+, bi.bib_id
+, l.location_code
+, ib.item_barcode
+, mi.item_enum
+from vger_report.tmp_hebraica_matches h
+left outer join ucladb.bib_index bi
+  on h.oclc_number = replace(bi.normal_heading, 'UCOCLC', '')
+  and bi.index_code = '0350'
+  and bi.normal_heading like 'UCOCLC%'
+left outer join ucladb.bib_mfhd bm
+  on bi.bib_id = bm.bib_id
+left outer join ucladb.mfhd_master mm
+  on bm.mfhd_id = mm.mfhd_id
+left outer join ucladb.location l
+  on mm.location_id = l.location_id
+left outer join ucladb.mfhd_item mi
+  on mm.mfhd_id = mi.mfhd_id
+left outer join ucladb.item_barcode ib
+  on mi.item_id = ib.item_id
+  and ib.barcode_status = 1 -- active
+where not regexp_like(location_code, '^.{2,6}sr$') -- "ghost" SRLF holdings
+and l.location_code not in ('in')
+and ib.item_barcode is not null
+order by oclc_number, item_enum
+;
+
+drop table vger_report.tmp_hebraica_matches purge;

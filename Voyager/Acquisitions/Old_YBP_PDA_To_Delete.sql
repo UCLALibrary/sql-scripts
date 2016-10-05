a05c1b9ef6ef17660adddc41ae689bb372e9d026
Andy Kohler <akohler@library.ucla.edu>
2016-10-05 13:50:07 -0800

    VBT-678: Remove exclusion of Law records

--- Voyager/Acquisitions/Old_YBP_PDA_To_Delete.sql
+++ Voyager/Acquisitions/Old_YBP_PDA_To_Delete.sql
@@ -5,6 +5,9 @@
     
     Initial deletion: 2014-10-30: out of 37251 old PDA, 27737 will be deleted
     Program will run daily starting 2014-10-31.
+    
+    2016-10-05: Per https://jira.library.ucla.edu/browse/VBT-678
+    no longer excluding Law records (916 $b LawDDA) from automatic deletion.
 */
 
 with ybp_pda_bibs as (
@@ -12,12 +15,6 @@ with ybp_pda_bibs as (
   from vger_subfields.ucladb_bib_subfield
   where tag = '982a'
   and subfield = 'YBP'
-)
-, ybp_law_pda_bibs as (
-  select record_id as bib_id
-  from vger_subfields.ucladb_bib_subfield
-  where tag = '916b'
-  and subfield = 'LawDDA'
 )
 select 
   y.bib_id
@@ -27,12 +24,7 @@ inner join ucladb.bib_master br on y.bib_id = br.bib_id
 inner join ucladb.bib_mfhd bm on br.bib_id = bm.bib_id
 inner join ucladb.mfhd_master mm on bm.mfhd_id = mm.mfhd_id
 inner join ucladb.location l on mm.location_id = l.location_id
-where not exists (
-  select * from ybp_law_pda_bibs
-  where bib_id = y.bib_id
-)
-and br.create_date < trunc(sysdate) - 730
--- 37251
+where br.create_date < trunc(sysdate) - 730
 and l.location_code = 'pdacq'
 and not exists (
   select * from ucladb.line_item 

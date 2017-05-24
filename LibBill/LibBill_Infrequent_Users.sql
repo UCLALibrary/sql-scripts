533a8bbe78598dd14bd4fc64cc9f947a06cac296
Andy Kohler <akohler@library.ucla.edu>
2017-05-24 15:46:28 -0800

    RR-275: Infrequent LibBill users

---
+++ LibBill/LibBill_Infrequent_Users.sql
@@ -0,0 +1,37 @@
+/*  LibBill users who have not changed anything invoice-related in the last 90 days.
+    RR-275
+*/
+
+with actions as (
+  select 'invoice' as tab, created_date, created_by from invoice
+  union all
+  select 'invoice_adjustment' as tab, created_date, created_by from invoice_adjustment
+  union all
+  select 'invoice_note' as tab, created_date, created_by from invoice_note
+  union all
+  select 'line_item' as tab, created_date, created_by from line_item
+  union all
+  select 'line_item_adjustment' as tab, created_date, created_by from line_item_adjustment
+  union all
+  select 'line_item_note' as tab, created_date, created_by from line_item_note
+  union all
+  select 'payment' as tab, payment_date as created_date, created_by from payment -- this one is different
+)
+, users as (
+  select
+    su.user_role
+  , su.user_name
+  , su.create_date
+  , ( select max(created_date) from actions where created_by = su.user_name) as most_recent
+  from staff_user su
+  where su.user_role is not null
+  and su.user_role not in ('inactive')
+)
+select *
+from users
+where most_recent is null
+or most_recent <= trunc(sysdate) - 90
+order by user_role, user_name
+;
+-- 51 rows
+

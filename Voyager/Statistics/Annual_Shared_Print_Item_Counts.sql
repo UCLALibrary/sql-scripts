37198799bf963d042a88d469f3150927bf1d17ae
Andy Kohler <akohler@library.ucla.edu>
2017-07-11 13:30:35 -0800

    RR-279: Update shared print figures for 2017

--- Voyager/Statistics/Annual_Shared_Print_Item_Counts.sql
+++ Voyager/Statistics/Annual_Shared_Print_Item_Counts.sql
@@ -47,6 +47,7 @@ and it.item_type_name = 'WEST Bronze'
 -- 26606 items 2014-08-27
 -- 26608 items 2015-07-31
 -- 26637 items 2016-07-25
+-- 26748 items 2017-07-10
 
 -- (B) Shared Print Monographic series
 with mfhds as (
@@ -78,3 +79,4 @@ and it.item_type_name = 'Shared Print in Place'
 -- 70 items 2014-08-27
 -- 100 items 2015-07-31
 -- 131 items 2016-07-25
+-- 168 items 2017-07-10

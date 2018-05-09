ed50344a4de0e4d4b0037efe8267eb4aa5cda320
Andy Kohler <akohler@library.ucla.edu>
2018-05-08 16:13:17 -0800

    RR-353: PromptCat shelf-ready assessment

---
+++ Voyager/Cataloging/PromptCat shelfready assessment RR-353.sql
@@ -0,0 +1,35 @@
+/*  PromptCat data for shelf-ready assessment.
+    RR-353
+*/
+
+-- Bib records where 910 $a starts with PromptCat
+with promptcat as (
+  select distinct
+    record_id as bib_id
+  from vger_subfields.ucladb_bib_subfield
+  where tag = '910a'
+  and subfield like 'PromptCat%'
+  --and record_id >= 5274085 -- first record created by promptcat
+)
+select 
+  pc.bib_id
+, trunc(bh.action_date) as create_date
+, coalesce(bh.encoding_level, '#') as enc_lvl -- show # for blank
+-- Days between bib creation and overlay by daily OCLC loader (if ever)
+, round( ( select min(action_date) from bib_history
+      where bib_id = bh.bib_id
+      and operator_id = 'uclaloader'
+    ) 
+  - bh.action_date
+) as days_to_catalog
+, vger_subfields.GetSubfields(pc.bib_id, '948a,948b') as f948ab
+, vger_subfields.GetSubfields(pc.bib_id, '981b,981c') as f981bc
+, vger_subfields.GetSubfields(pc.bib_id, '982b') as f982b
+from promptcat pc
+inner join bib_history bh
+  on pc.bib_id = bh.bib_id
+  and bh.action_type_id = 1 -- Create
+  and bh.operator_id = 'promptcat'
+order by pc.bib_id
+;
+

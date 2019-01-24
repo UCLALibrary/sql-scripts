7f23d4138f8acfd97e0d88713d6d377f629a9eee
Andy Kohler <akohler@library.ucla.edu>
2019-01-23 16:09:24 -0900

    RR-428: Re-run report with new list of bibs and new dates

--- Voyager/Circulation/Powell Reserves Usage Data RR-339.sql
+++ Voyager/Circulation/Powell Reserves Usage Data RR-339.sql
@@ -1,8 +1,11 @@
 /*  Circ info for selected bib titles.
     Bib ids imported from provided excel file: 105 rows, 103 distinct values
-    Dups: 7121740, 8266817
-    RR-339
+      Dups: 7121740, 8266817
+      RR-339
+    20190123: Ran with new list of bib ids for RR-428, importing 137 distinct bib ids.
 */
+select count(*), count(distinct bib_id) from vger_report.tmp_rr_339;
+
 create index vger_report.ix_tmp_rr_339 on vger_report.tmp_rr_339 (bib_id);
 
 select distinct 
@@ -17,25 +20,25 @@ select distinct
 , ( select count(*)
     from ucladb.circcharges_vw
     where item_id = mi.item_id
-    and charge_date_time between to_date('20170925', 'YYYYMMDD') and to_date('20171215 235959', 'YYYYMMDD HH24MISS')
+    and charge_date_time between to_date('20180924', 'YYYYMMDD') and to_date('20181214 235959', 'YYYYMMDD HH24MISS')
 ) as charges
 , i.historical_browses as "BROWSES*"
 , ( select count(*)
     from ucladb.hold_recall_item_archive
     where item_id = mi.item_id
-    and hold_recall_status_date between to_date('20170925', 'YYYYMMDD') and to_date('20171215 235959', 'YYYYMMDD HH24MISS')
+    and hold_recall_status_date between to_date('20180924', 'YYYYMMDD') and to_date('20181214 235959', 'YYYYMMDD HH24MISS')
     and hold_recall_type = 'R'
 ) as recalls
 , ( select count(*)
     from ucladb.hold_recall_item_archive
     where item_id = mi.item_id
-    and hold_recall_status_date between to_date('20170925', 'YYYYMMDD') and to_date('20171215 235959', 'YYYYMMDD HH24MISS')
+    and hold_recall_status_date between to_date('20180924', 'YYYYMMDD') and to_date('20181214 235959', 'YYYYMMDD HH24MISS')
     and hold_recall_type = 'H'
 ) as holds
 , ( select coalesce(sum(reserve_charges), 0)
     from ucladb.reserve_item_history
     where item_id = mi.item_id
-    and effect_date between to_date('20170925', 'YYYYMMDD') and to_date('20171215 235959', 'YYYYMMDD HH24MISS')
+    and effect_date between to_date('20180924', 'YYYYMMDD') and to_date('20181214 235959', 'YYYYMMDD HH24MISS')
 ) as reserves
 from vger_report.tmp_rr_339 b
 inner join ucladb.bib_text bt on b.bib_id = bt.bib_id
@@ -49,3 +52,4 @@ left outer join ucladb.item_barcode ib on i.item_id = ib.item_id and ib.barcode_
 order by title_brief, pub_date, barcode
 ;
 
+drop table vger_report.tmp_rr_339 purge;

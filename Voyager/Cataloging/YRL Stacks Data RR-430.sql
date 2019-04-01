6821cef54d57d5512e34b83ae894537b84adecca
Andy Kohler <akohler@library.ucla.edu>
2019-04-01 10:37:58 -0800

    RR-430: Add OCLC holdings / NRLF info to Voyager data and re-export

--- Voyager/Cataloging/YRL Stacks Data RR-430.sql
+++ Voyager/Cataloging/YRL Stacks Data RR-430.sql
@@ -66,6 +66,8 @@ and mm.suppress_in_opac = 'N'
 and mm.normalized_call_no is not null
 --order by l.location_code, mm.normalized_call_no
 ;
+create index vger_report.ix_tmp_rr_430_mfhd on vger_report.tmp_rr_430 (mfhd_id);
+create index vger_report.ix_tmp_rr_430_oclc on vger_report.tmp_rr_430 (oclc);
 
 select sum(mfhd_count) from ucladb.location l where l.location_code in ('yr', 'yr*', 'yr**', 'yr***', 'yrncrc', 'yrpe', 'yrper');
 -- 1550864 20190205
@@ -95,24 +97,139 @@ having count(*) > 50
 order by num desc
 ;
 
--- Format / Classification files
--- Serials: 55291
--- Mono sets: 53560
--- Monos (single): who knows... no sheet for O, X
-select *
-from vger_report.tmp_rr_430
---where bib_lvl = 's'
---where bib_lvl = 'm' and (dt_status = 'm' or summ_hlds = 'X')
-where bib_lvl = 'm' and (dt_status != 'm' and summ_hlds is null)
-and normalized_call_no not in ('SRLF', 'SEE INDIVIDUAL RECORDS FOR CALL NUMBERS', 'SUPPRESSED', 'IN PROCESS')
---and normalized_call_no like 'Z%'
-and not regexp_like(normalized_call_no, '^[A-Z]')
+/*  Create supporting table with YRL/SRLF item counts for serials; faster to do this
+    once up front than for each row in ~30 queries below.
+*/
+create table vger_report.tmp_rr_430_serials as
+select
+  b.bib_id
+, b.mfhd_id
+, ( select count(*) from ucladb.mfhd_item where mfhd_id = b.mfhd_id ) as yrl_items
+, ( select count(*) 
+    from ucladb.bib_mfhd bm 
+    inner join ucladb.mfhd_master mm on bm.mfhd_id = mm.mfhd_id
+    inner join ucladb.location l on mm.location_id = l.location_id
+    inner join ucladb.mfhd_item mi on mm.mfhd_id = mi.mfhd_id
+    where bm.bib_id = b.bib_id
+    and l.location_code like 'sr%'
+) as sr_items    
+from vger_report.tmp_rr_430 b
+where b.bib_lvl = 's'
+;
+create index vger_report.ix_tmp_rr_430_serials on vger_report.tmp_rr_430_serials (mfhd_id);
+
+select count(*), count(distinct bib_id) from vger_report.tmp_rr_430_serials;
+-- 56905 rows, on 56509 bibs
+
+-- Table for checked OCLC numbers
+create table vger_report.tmp_rr_430_checked (
+  oclc varchar2(12) not null
+, oclc_holdings int not null
+, held_by_nrlf char(1) not null
+)
+;
+-- Import data from server: 1275417 rows
+-- grep -h [YN]$ done/*.out | sort -u > checked_good.lst
+create index vger_report.ix_tmp_rr_430_checked on vger_report.tmp_rr_430_checked (oclc);
+
+-- Clean-up a few zero-left-padded OCLC numbers
+update vger_report.tmp_rr_430 set oclc = ltrim(oclc, '0') where oclc like '0%';
+update vger_report.tmp_rr_430_checked set oclc = ltrim(oclc, '0') where oclc like '0%';
+-- And a lot of space-padded OCLC numbers from API program....
+delete from vger_report.tmp_rr_430_checked where trim(oclc) is null;
+
+update vger_report.tmp_rr_430_checked set oclc = trim(oclc) where oclc like '% %';
+commit;
+
+-- Did various cleanup; there are 50 left which either trigger errors (bad JSON), or legitimately have 0 holdings.
+select count(*) from tmp_rr_430_checked where oclc_holdings = 0;
+--delete from vger_report.tmp_rr_430_checked where oclc_holdings = 0;
+--commit;
+
+/***********************************************************
+  Generate Excel reports
+  
+  Format / Classification files
+  -- Serials: 56905
+  -- Mono sets: 54022
+  -- Monos (single): who knows... no sheet for O, X
+***********************************************************/
+select
+  r.oclc
+, r.bib_id
+, r.permalink
+, r.mfhd_id
+, r.location_code
+--, s.yrl_items -- only for serials
+, r.call_no_type
+, r.normalized_call_no
+, r.display_call_no
+, r.other_locs
+, r.srlf_locs
+--, s.sr_items as srlf_items -- only for serials
+, n.held_by_nrlf
+, n.oclc_holdings
+, r.bib_lvl
+, r.record_type
+, r.govt_pub
+, r.place_code
+, r.language
+, r.dt_status
+, r.date1
+, r.date2
+, r.author
+, r.title
+, r.pub_info
+, r.physical_extent
+, r.summ_hlds
+, r.other_form
+, r.urls
+from vger_report.tmp_rr_430 r
+--left outer join vger_report.tmp_rr_430_serials s on r.mfhd_id = s.mfhd_id -- only for serials
+left outer join vger_report.tmp_rr_430_checked n on r.oclc = n.oclc
+where normalized_call_no not in ('SRLF', 'SEE INDIVIDUAL RECORDS FOR CALL NUMBERS', 'SUPPRESSED', 'IN PROCESS')
+--and bib_lvl = 's' -- Serials, all
+--and bib_lvl = 'm' and (dt_status = 'm' or summ_hlds = 'X') -- Mono sets, all
+and bib_lvl = 'm' and (dt_status != 'm' and summ_hlds is null) -- Monos (single), letter by letter, including non-letters for errors
+--and normalized_call_no like 'Z%' -- change to each letter for separate export
+and not regexp_like(normalized_call_no, '^[A-Z]') -- non-letters for errors
 order by normalized_call_no
 ;
 
 -- By YRL floors: 3rd (A-E), 4th (F-P), 5th (PA-Z)
-select *
-from vger_report.tmp_rr_430
+select
+  r.oclc
+, r.bib_id
+, r.permalink
+, r.mfhd_id
+, r.location_code
+, s.yrl_items -- only for serials
+, r.call_no_type
+, r.normalized_call_no
+, r.display_call_no
+, r.other_locs
+, r.srlf_locs
+, s.sr_items as srlf_items -- only for serials
+, n.held_by_nrlf
+, n.oclc_holdings
+, r.bib_lvl
+, r.record_type
+, r.govt_pub
+, r.place_code
+, r.language
+, r.dt_status
+, r.date1
+, r.date2
+, r.author
+, r.title
+, r.pub_info
+, r.physical_extent
+, r.summ_hlds
+, r.other_form
+, r.urls
+from vger_report.tmp_rr_430 r
+left outer join vger_report.tmp_rr_430_serials s on r.mfhd_id = s.mfhd_id -- only for serials, but have to include on floor-based reports
+left outer join vger_report.tmp_rr_430_checked n on r.oclc = n.oclc
 where normalized_call_no not in ('SRLF', 'SEE INDIVIDUAL RECORDS FOR CALL NUMBERS', 'SUPPRESSED', 'IN PROCESS')
 --and regexp_like(normalized_call_no, '^[A-E]') -- 3rd floor
 --and (regexp_like(normalized_call_no, '^[F-O]') or regexp_like(normalized_call_no, '^P ')) --4th floor
@@ -131,3 +248,24 @@ order by bib_lvl
 
 -- Clean up
 drop table vger_report.tmp_rr_430 purge;
+drop table vger_report.tmp_rr_430_checked purge;
+drop table vger_report.tmp_rr_430_serials purge;
+
+-- Compare
+-- 9301 in base table have no OCLC number, thus no match
+select count(*)
+from vger_report.tmp_rr_430 r
+where not exists (
+  select * 
+  from vger_report.tmp_rr_430_checked
+  where oclc = r.oclc
+)
+--and oclc is not null
+;
+
+select count(distinct oclc) from vger_report.tmp_rr_430; -- 1481920
+select count(distinct oclc) from vger_report.tmp_rr_430_checked; -- 1275417
+
+select * from vger_report.tmp_rr_430_checked where oclc_holdings = 0;
+select * from vger_report.tmp_rr_430 where oclc = '992003054';
+

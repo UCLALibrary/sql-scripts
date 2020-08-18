ed4d72ab59185cc416b37a8fb0031d132fcaa3d7
Andy Kohler <akohler@library.ucla.edu>
2020-08-18 14:35:31 -0800

    VBT-1652: Clark bibs with 655s, with and without non-Clark holdings

---
+++ Voyager/Cataloging/Clark 655s VBT-1652.sql
@@ -0,0 +1,47 @@
+/*  Clark Library bibs with 655 fields.
+    Two reports: 
+    #1: Only Clark holdings
+    #2: Clark holdings and any others
+    
+    VBT-1652
+*/
+
+with bibs as (
+  select distinct
+    record_id as bib_id
+  , field_seq
+  , indicators
+  from vger_subfields.ucladb_bib_subfield
+  where tag like '655%'
+)
+, clark as (
+  select 
+    bm.bib_id
+  , bm.mfhd_id
+  , b.field_seq
+  , b.indicators
+  from bibs b
+  inner join bib_mfhd bm on b.bib_id = bm.bib_id
+  inner join mfhd_master mm on bm.mfhd_id = mm.mfhd_id
+  inner join location l on mm.location_id = l.location_id
+  where l.location_code like 'ck%'
+)
+--select count(distinct bib_id) from clark c;
+--, d as (
+select distinct
+  c.bib_id
+, replace(c.indicators, ' ', '_') as indicators
+, vger_subfields.GetFieldFromSubfields(c.bib_id, c.field_seq) as f655
+from clark c
+-- #1: where not exists (
+-- #2: where exists (
+where exists (
+  select *
+  from bib_mfhd
+  where bib_id = c.bib_id
+  and mfhd_id not in (select mfhd_id from clark)
+)
+order by bib_id
+--) select count(distinct bib_id) from d
+;
+-- 10797 bibs total: 2347 with others, 8450 without
